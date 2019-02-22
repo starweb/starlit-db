e101f4659af098b1a66e704333915c7286f0a9ec
Patrick Rodacker <patrick@rodacker.de>
2019-02-22 11:13:49 +0100

    adds a PdoFactoryInterface and default PdoFactory

---
+++ src/PdoFactory.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Starlit Db.
+ *
+ * @copyright Copyright (c) 2016 Starweb AB
+ * @license   BSD 3-Clause
+ */
+
+namespace Starlit\Db;
+
+use PDO;
+
+class PdoFactory implements PdoFactoryInterface
+{
+    public function createPdo(string $dsn, string $username = null, string $password = null, array $options = []): PDO
+    {
+        return new PDO($dsn, $username, $password, $options);
+    }
+}
